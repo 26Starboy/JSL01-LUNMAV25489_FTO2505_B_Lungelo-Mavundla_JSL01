14282a04a897f7571d73ad76304bd32b7e52874d
Lungelo_Mavundla <staboypretty333@gmail.com>
2025-07-12 16:50:40 +0200

    Documented references and resources used in project
    all links that helped completing the project

---
+++ REFERENCE.s
@@ -0,0 +1,11 @@
+✅ 1. Font Used Plus Jakarta Sans – Google Fonts🔗 https://fonts.google.com/specimen/Plus+Jakarta+Sans
+✅ 2. Favicon Generator 🔗 https://realfavicongenerator.net/ 🔗 https://favicon.io/
+✅ 3. CSS Documentation 🔗 https://developer.mozilla.org/en-US/docs/Web/CSS/Reference
+✅ 4. HTML Documentation 🔗 https://developer.mozilla.org/en-US/docs/Web/HTML 
+✅ 5. Flexbox Reference 🔗 https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+✅ 6. roadmap.sh 🔗 https://roadmap.sh/
+✅ 7. CSS Grid Reference 🔗 https://css-tricks.com/snippets/css/complete-guide-grid/
+✅ 8. Dark Mode Guide (CSS Variables) 🔗 https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties 🔗 https://web.dev/prefers-color-scheme/
+✅ 9. Emojis for Web Content (used in task labels) 🔗 https://emojipedia.org/
+✅ 10.Inspiration / Kanban Concepts  🔗 https://www.figma.com/design/y7bFCUYL5ZHfPeojACBXg2/Challenges-%7C-JSL?node-id=6033-10647&t=OzZAM1iiXP4DMCUw-0
+
